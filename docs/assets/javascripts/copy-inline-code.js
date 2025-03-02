842b00007f4abcd59074b49632bf20b67d90280a
adminus <adminus@adminus.adminus>
2025-03-02 04:38:30 +0300

    autocommit

--- docs/assets/javascripts/copy-inline-code.js
+++ docs/assets/javascripts/copy-inline-code.js
@@ -2,12 +2,12 @@ document.addEventListener("DOMContentLoaded", function () {
     document.querySelectorAll("code").forEach((codeBlock) => {
         if (codeBlock.parentElement.tagName !== "PRE") { // Только для инлайн-кода
             codeBlock.style.cursor = "pointer";
-            codeBlock.title = "Кликните для копирования";
+            codeBlock.setAttribute("data-tooltip", "Кликните для копирования");
 
             codeBlock.addEventListener("click", () => {
                 navigator.clipboard.writeText(codeBlock.innerText).then(() => {
-                    codeBlock.title = "Скопировано!";
-                    setTimeout(() => (codeBlock.title = "Кликните для копирования"), 900);
+                    codeBlock.setAttribute("data-tooltip", "Скопировано!");
+                    setTimeout(() => codeBlock.setAttribute("data-tooltip", "Кликните для копирования"), 1000);
                 });
             });
         }
