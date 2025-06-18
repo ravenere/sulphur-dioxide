40a95651d3c2f2f5e5cd20eaebfb95199b165fd7
raven <raven@pastercpp.t.me>
2025-06-18 11:48:14 +0300

    autocommit

--- dply.bat
+++ dply.bat
@@ -3,5 +3,5 @@ git commit -m "autocommit"
 
 git push
 
-python -m gh-deploy --clean --force --remote-name https://github.com/ravenere/sulphur-dioxide.git
+mkdocs.exe gh-deploy --clean --force --remote-name https://github.com/ravenere/sulphur-dioxide.git
 pause
